6b6520de84fd7cc73d6ebe24826ad94011567e90
Gamal Abdulsalam <gamalalejandro@gmail.com>
2024-03-01 22:17:03 +0100

    managed to apply parallelism to the brute force computation

--- compute_nn_brute_force.py
+++ compute_nn_brute_force.py
@@ -1,8 +1,14 @@
+import numpy as np
 import sys
+from multiprocessing import Pool
+import multiprocessing 
 from personal_hnsw import HNSW
 from helpers.glove_helpers import (
-    brute_force_nn, 
+    brute_force_parallel,
     load_glove,
+    write_brute_force_nn,
+    load_brute_force,
+    split
 )
 
 
@@ -10,10 +16,24 @@ if __name__ == '__main__':
     dim, limit = int(sys.argv[1]), int(sys.argv[2])
 
     embeddings = load_glove(dim=dim, limit=limit)
-    brute_force_nn(
-        n=10, 
-        index=HNSW(), 
-        embeddings=embeddings,
-        limit=limit,
-        dim=dim
-    )
+
+    num_processes = multiprocessing.cpu_count()
+
+    splits, nb_per_split = split(embeddings, num_processes)
+    splits = [(10, embeddings, split, limit, dim, nb_per_split, i) for i, split in enumerate(splits)]
+
+    with Pool(processes=num_processes) as pool:
+        results = pool.starmap(brute_force_parallel, splits)
+
+    nearest_neighbors = {}
+    for result in results:
+        for idx, neighbors in result.items():
+            nearest_neighbors[idx] = neighbors
+
+    true_nn = load_brute_force(dim, limit)
+
+    n = np.random.randint(0, len(true_nn.keys()))
+    test_neighbors = true_nn[n]
+    parallel_neighbors = nearest_neighbors[n]
+    print(test_neighbors)
+    print(parallel_neighbors)
--- helpers/glove_helpers.py
+++ helpers/glove_helpers.py
@@ -21,6 +21,7 @@ def ann(index, sample, ef=10):
         ann_time
     )
 
+
 def load_brute_force(dim, limit):
     path = f'/home/gamal/glove_dataset/brute_force/lim_{limit}_dim_{dim}'
     with open(path, 'rb') as file:
@@ -50,9 +51,9 @@ def load_glove(dim=50, limit=None, include_words=False):
 
     return (np.array(embeddings), words) if include_words else np.array(embeddings) 
 
+
 def brute_force_nn(
         n: int, 
-        index: HNSW, 
         embeddings: np.array, 
         limit=None, 
         dim=50
@@ -68,30 +69,83 @@ def brute_force_nn(
     else:
         total = 400_000
 
+        nearest_neighbors = {}
+        for idx in tqdm(range(embeddings.shape[0]), total=embeddings.shape[0]):
+
+            dists_vector = get_distance(embeddings[idx], embeddings, b_matrix=True)
+            dists_vector = [(jdx, dist) for jdx, dist in enumerate(dists_vector)]
+
+            dists_vector = sorted(
+                dists_vector,
+                key=lambda x: x[1]
+            )[1:n+1]
+
+            nearest_neighbors[idx] = dists_vector
+
+    return None
+
+def brute_force_parallel(
+    n: int,
+    all_emb: np.array,
+    emb: np.array,
+    limit=None,
+    dim=50,
+    per_split: int = None,
+    split_nb: int = None
+):
+    
+    if limit:
+        total = limit
+    else:
+        total = 400_000
+
+    to_add = split_nb*per_split
     nearest_neighbors = {}
-    for idx in tqdm(range(embeddings.shape[0]), total=total, desc='Brute force finding NNs'):
+    if split_nb == 0:
+        for idx in tqdm(
+            range(emb.shape[0]), 
+            total=emb.shape[0],
+        ):
 
-        dists_vector = index.get_distance(embeddings[idx], embeddings, b_matrix=True)
-        dists_vector = [(jdx, dist) for jdx, dist in enumerate(dists_vector)]
+            dists_vector = get_distance(emb[idx], all_emb, b_matrix=True)
+            dists_vector = [(jdx, dist) for jdx, dist in enumerate(dists_vector)]
 
-        dists_vector = sorted(
-            dists_vector,
-            key=lambda x: x[1]
-        )[1:n+1]
+            dists_vector = sorted(
+                dists_vector,
+                key=lambda x: x[1]
+            )[1:n+1]
 
-        nearest_neighbors[idx] = dists_vector
+            idx += to_add
+            nearest_neighbors[idx] = dists_vector
+    else:
+        for idx in range(emb.shape[0]): 
 
+                dists_vector = get_distance(emb[idx], all_emb, b_matrix=True)
+                dists_vector = [(jdx, dist) for jdx, dist in enumerate(dists_vector)]
+
+                dists_vector = sorted(
+                    dists_vector,
+                    key=lambda x: x[1]
+                )[1:n+1]
+
+                idx += to_add
+                nearest_neighbors[idx] = dists_vector
+
+    return nearest_neighbors
+    
+
+def write_brute_force_nn(nearest_neighbors: dict[list]):
     path = f'/home/gamal/glove_dataset/brute_force/lim_{limit}_dim_{dim}'
     with open(path, 'wb') as file:
         pickle.dump(nearest_neighbors, file)
 
-    return None
 
 def get_distance(a, b, b_matrix=False):
     if not b_matrix:
         return np.linalg.norm(a-b)
     else:
         return np.linalg.norm(a-b, axis=1)
+
     
 def get_measures(nearest_to_queries, nearest_to_queries_ann):
 
@@ -102,3 +156,21 @@ def get_measures(nearest_to_queries, nearest_to_queries_ann):
             measures['recall@10'].append(ann in true_nns)
 
     return np.array(measures['recall@10'])
+
+
+def split(queries, num_splits):
+    per_split = queries.shape[0] // num_splits
+
+    splits = []
+    buffer =0
+    for i in range(num_splits):
+        splits.append(queries[buffer:buffer+per_split, :])
+        buffer += per_split
+
+        if i == (num_splits - 1): break
+
+    splits[-1] = queries[buffer:, :]
+
+    return splits, per_split
+
+
